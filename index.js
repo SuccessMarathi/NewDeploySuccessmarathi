3af15d4c3fd162998601b812af51591d7f6781db
DevidasKale480 <devidas.kale@kritikasoftware.com>
2025-02-22 13:29:21 +0530

    Deployment

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send("Server is working");
@@ -39,7 +39,8 @@ app.use("/api", userRoutes);
 app.use("/api", courseRoutes);
 app.use("/api", adminRoutes);
 
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${5000}`);
+  console.log(`Server is running on http://localhost:${port}`); // Use PORT here
   connectDb();
 });
