e6d034c0b3b05c6d1ff56c54503bfaea99014813
SuccessMarathi <akashrathod782304@gmail.com>
2025-05-21 07:54:00 +0530

    Update course.js

--- controllers/course.js
+++ controllers/course.js
@@ -409,10 +409,10 @@ export const verifyPayment = TryCatch(async (req, res) => {
   // Define earnings for each course
    const earningsMapping = {
      "67b81fdeb7e36f5e02b649cd": { referrer: 160, grandReferrer: 1 },
-     "67b82012b7e36f5e02b649cf": { referrer: 280, grandReferrer: 40 },
+     "67b82012b7e36f5e02b649cf": { referrer: 1, grandReferrer: 1 },
      "67b82046b7e36f5e02b649d1": { referrer: 490, grandReferrer: 70 },
      "67b8206eb7e36f5e02b649d3": { referrer: 700, grandReferrer: 100 },
-    "67b82092b7e36f5e02b649d5": { referrer: 1, grandReferrer: 1 },
+    "67b82092b7e36f5e02b649d5": { referrer: 1605, grandReferrer: 220 },
      "67b820b1b7e36f5e02b649d7": { referrer: 3650, grandReferrer: 399 },
    };
 
