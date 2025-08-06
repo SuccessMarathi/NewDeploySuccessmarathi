83565684cc283d81ad7a8f3175172438f5588303
Speedysays01 <surabhimhamane05@gmail.com>
2025-08-06 13:56:59 +0530

    getweek added

--- controllers/admin.js
+++ controllers/admin.js
@@ -403,6 +403,12 @@ const updateEarnings = (user, amount) => {
   user.earnings.lastUpdated = now;
 };
 
+Date.prototype.getWeek = function () {
+  const oneJan = new Date(this.getFullYear(), 0, 1);
+  const numberOfDays = Math.floor((this - oneJan) / (24 * 60 * 60 * 1000));
+  return Math.ceil((this.getDay() + 1 + numberOfDays) / 7);
+};
+
 
 export const handlePendingOrder = TryCatch(async (req, res) => {
   const { orderId, action } = req.body;
